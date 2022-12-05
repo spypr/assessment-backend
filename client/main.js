e9dfba1d35e815ef2abab74533b7315e92af59ae
Steven Pyper <steven.pyper@gmail.com>
2022-12-04 21:12:06 -0700

    updated changes, compliment and fortune work now -Steve

--- client/main.js
+++ client/main.js
@@ -4,10 +4,6 @@ const form = document.querySelector('form')
 
 const baseURL = `http://localhost:4000/api/users`
 
-const createUser = body => axios.post(baseURL, body).then(usersCallback).catch(errCallback)
-const deleteName = id => axios.delete(`${baseURL}/${id}`).then(userCallback).catch(errCallback)
-const updateUser = (id, type) => axios.put(`${baseURL}/${id}`, {type}).then(usersCallback).catch(errCallback)
-
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
         .then(res => {
@@ -23,26 +19,6 @@ const getFortune = () => {
             alert(data);
     });
 };
-
-function submitHandler(e) {
-    e.preventDefault()
-
-    let name = document.querySelector('#name')
-    let age = document.querySelector('#age')
-
-    let bodyObj = {
-        name: name.value,
-        age: age.value, 
-    }
-
-    createUser(bodyObj)
-
-    name.value = ''
-    age.value = ''
-};
-
-
-
 
 complimentBtn.addEventListener('click', getCompliment)
 fortuneBtn.addEventListener('click', getFortune)
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const {createUser, updateUser, deleteUser} = require('./controller')
 
 const app = express();
 
@@ -14,9 +13,6 @@ const { getFortune } = require('./controller');
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune);
 
-app.post('/api/user',createUser)
-app.put('/api/user/:id', updateUser)
-app.delete('/api/user/:id', deleteUser)
 
 app.listen(4000, () => console.log("Server running on 4000"));
 
