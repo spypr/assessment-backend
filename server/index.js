92aec6268c946a69b2bf4787a8a0cef67950006e
Steven Pyper <117680963+spypr@users.noreply.github.com>
2022-12-10 17:27:27 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,15 +4,15 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
-const { getCompliment, createUser } = require('./controller');
-const { getFortune } = require('./controller');
-app.get("/api/users",createUser);
-app.get("/api/compliment", getCompliment);
-app.get("/api/fortune", getFortune);
+const { getCompliment, getFortune, createUser, updateUser, deleteUser } = require('./controller')
 
+app.get("/api/users",createUser)
+app.get("/api/compliment", getCompliment)
+app.get("/api/fortune", getFortune)
+app.post("/api/users", updateUser)
+app.delete("/api/users/:id", deleteUser)
 
 app.listen(4000, () => console.log("Server running on 4000"));
 
